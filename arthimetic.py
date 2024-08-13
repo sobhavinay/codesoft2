84ba5f19d586c9976de2b3c90b6ab9a1eb62e83d
Sobha Vinay Babu <vinaymerugumala1@gmail.com>
2024-08-13 19:09:32 +0530

    Add files via upload

---
+++ arthimetic.py
@@ -0,0 +1,14 @@
+a=int(input("enter a number"))
+op=input("enter a operator")
+b=int(input("enter a number"))
+if op == "+":
+    print(a+b)
+elif op=="-":
+    print(a-b)
+elif op=="*":
+    print(a*b)
+elif op== "/":
+    print(a/b)
+else:
+    print("enter correct operator")    
+    
